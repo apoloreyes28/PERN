dfc1e289b7f70ec431f5e5499320fb3c1507051f
apoloreyes28 <reyesapolinar31@gmail.com>
2024-02-22 17:24:45 -0700

    Construir el boton de Delete

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -5,6 +5,20 @@ const ListTodos = () => {
     const [todos, setTodos] = useState([]);
     // definimos el arreglo vacío para poder almacenar los datos dentro
 
+    // delete todo function
+    const deleteTodo = async(id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+            method: "DELETE"
+            });
+
+            // console.log(deleteTodo);
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     const getTodos = async() => {
         try {
             const response = await fetch("http://localhost:5000/todos");
@@ -43,10 +57,12 @@ const ListTodos = () => {
                     </tr>
                     */}
                     {todos.map(todo => (
-                        <tr>
+                        <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
                             <td>Edit</td>
-                            <td>Delete</td>
+                            <td>
+                                <button className='btn btn-danger' onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
